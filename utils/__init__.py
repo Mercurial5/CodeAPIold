147a2b73290ccdd9b4151b6663d362522dc607bd
mercurial <dias.nespayev@gmail.com>
2022-09-17 14:28:04 +0600

    feat: check code with tests

---
+++ utils/__init__.py
@@ -0,0 +1,19 @@
+from utils.exceptions import SecurityError
+from utils.Executor import Executor
+
+
+def check(code: str, io: list):
+    try:
+        executor = Executor('py', code)
+    except SecurityError as e:
+        return dict(status=False, reason=str(e))
+
+    for index, io_item in enumerate(io, 1):
+        response = executor.run(io_item.get('input', ''))
+        if not response['status']:
+            return response.update(case=index)
+
+        if response['result'].strip() != io_item['output']:
+            return dict(status=False, reason='Wrong answer', case=index)
+
+    return dict(status=True)
