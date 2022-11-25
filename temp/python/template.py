4b2152caf90e89659f5c552133bbd610932c26e3
Nikita Mordvintsev <90397176+Tunaxx-New@users.noreply.github.com>
2022-11-25 17:07:42 +0600

    fix: added EOFError try except block

--- temp/python/template.py
+++ temp/python/template.py
@@ -4,12 +4,14 @@ exit = 1
 quit = 1
 os._exit = 1
 sys.exit = 1
-i = 0
-for _ in range(%s):
 
-    %s
+try:
 
-    print('TestCase')
-    i += 1
-    if i == 0:
-        1 / 0
+    for _ in range(%s):
+
+        %s
+
+        print('TestCase')
+
+except EOFError:
+    pass
